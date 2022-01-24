520dc544acba6574b1c47a3ce5ed063265e09945
Regan Mudhar <rm811@emps-gv3.ex.ac.uk>
2022-01-24 17:35:10 +0000

    updated script for plotting

--- open_differences.py
+++ open_differences.py
@@ -11,10 +11,10 @@ from shared_functions import *
 
 def plots(ds1, ds2, exp_name):
     # Set-up variables from the dataset
-    lat = ds.coords['lat'].data
-    lon = ds.coords['lon'].data
-    p = ds.coords['pfull'].data
-    upper_p = ds.coords['pfull'].sel(pfull=1, method='nearest') # in order to cap plots at pressure = 1hPa
+    lat = ds1.coords['lat'].data
+    lon = ds1.coords['lon'].data
+    p = ds1.coords['pfull'].data
+    upper_p = ds1.coords['pfull'].sel(pfull=1, method='nearest') # in order to cap plots at pressure = 1hPa
     z = altitude(p)
     H = 8 #scale height km
     p0 = 1000 #surface pressure hPa    
@@ -23,9 +23,11 @@ def plots(ds1, ds2, exp_name):
     uz = diff_variables(ds1, ds2, lat, lon, z, p)
 
     fig1 = plt.figure(figsize=(10,8))
+    lvls1 = np.arange(-20,22.5,2.5)
     ax1 = fig1.add_subplot(111)
-    cs1 = uz.plot.contourf(levels=21, cmap='RdBu_r')
-    ax1.contourf(cs1, colors='gainsboro', linewidths=1)
+    cs1 = uz.plot.contourf(levels=lvls1, cmap='RdBu_r', add_colorbar=False)
+    ax1.contour(cs1, colors='gainsboro', linewidths=1)
+    plt.colorbar(cs1, label=r'Difference (ms$^{-1}$)')
     plt.xlabel('Latitude', fontsize='large')
     plt.xlim(-90,90)
     plt.xticks([-90, -45, 0, 45, 90], ['90S', '45S', '0', '45N', '90N'])
@@ -38,11 +40,11 @@ def plots(ds1, ds2, exp_name):
 
 if __name__ == '__main__': 
     #Set-up data to be read in
-    exp = ['PK_eps0_vtx1_zoz13_7y','PK_eps10_vtx1_zoz13_7y']
+    exp = ['PK_eps0_vtx4_zoz18_7y','PK_eps10_vtx4_zoz18_7y']
     time = 'daily'
     years = 0 # user sets no. of years worth of data to ignore due to spin-up
     ds1 = discard_spinup1(exp[0], time, '_interp', years)
     ds2 = discard_spinup1(exp[1], time, '_interp', years)
-    exp_name = 'PK_eps0-10_zoz13_vtx1'
+    exp_name = 'PK_eps0-10_zoz18_vtx4'
 
     plots(ds1, ds2, exp_name)
