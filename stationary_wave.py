921a6d6d5756e532472b870ff1911a46709c95fd
rgnmudhar <51166269+rgnmudhar@users.noreply.github.com>
2022-05-23 16:41:16 +0100

    Update stationary_wave.py
    Updated file

--- stationary_wave.py
+++ stationary_wave.py
@@ -18,15 +18,17 @@ def plot(ds, x, p, lvls):
     cs = ax.contourf(ds.coords['lon'].data, ds.coords['lat'].data, x,\
         cmap='RdBu_r', levels=lvls, transform = ccrs.PlateCarree())
     cb = plt.colorbar(cs, pad=0.1)
-    cb.set_label(label='Geopotential Height Anomaly (m)', size='large')
+    cb.set_label(label='Geopotential Height Anomaly (m)', size='x-large')
+    cb.ax.tick_params(labelsize='x-large')
     ln = ax.contour(asym.coords['lon'].data, asym.coords['lat'].data, heat,\
         levels=11, colors='k', linewidths=0.5, alpha=0.4, transform = ccrs.PlateCarree())
     #ax.coastlines()
     ax.set_global()
     ax.gridlines(draw_labels=True, dms=True, x_inline=False, y_inline=False)
     ax.set_extent([-180, 180, 0, 90], crs=ccrs.PlateCarree())
-    plt.title(r'$\sim$' + '{0:.0f} hPa Geopotential Height Anomaly'.format(p), fontsize='x-large')
-    return plt.show()
+    #plt.title(r'$\sim$' + '{0:.0f} hPa Geopotential Height Anomaly'.format(p), fontsize='xx-large')
+    plt.savefig('gph.pdf', bbox_inches = 'tight')
+    return plt.close()
 
 exp = 'PK_e0v4z13_q6m2y45l800u200'
 time = 'daily'
@@ -38,4 +40,4 @@ ds = xr.open_mfdataset(files, decode_times=False)
 gph_t = ds.height.mean(dim='time')
 
 plot(ds, anomaly(gph_t.sel(pfull=500, method='nearest')), 500, np.arange(-120,130,10))
-plot(ds, anomaly(gph_t.sel(pfull=10, method='nearest')), 10, np.arange(-250,260,10))
+#plot(ds, anomaly(gph_t.sel(pfull=10, method='nearest')), 10, np.arange(-250,260,10))
