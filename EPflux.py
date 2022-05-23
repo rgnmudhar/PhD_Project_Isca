a14ab948b4baf1aa8fc4d2a8e7f9a28f9ca06b11
rgnmudhar <51166269+rgnmudhar@users.noreply.github.com>
2022-05-23 16:36:03 +0100

    Update EPflux.py
    Enabled difference plots

--- EPflux.py
+++ EPflux.py
@@ -21,37 +21,80 @@ def calc_ep(ds):
     uz = u.mean(dim='time').mean(dim='lon')
     return uz, div, ep1, ep2
 
-#Set-up data to be read in
-exp = 'PK_e0v4z13' #_q6m2y45l800u200' #_w15a4p800f800g50'
-time = 'daily'
-years = 2 # user sets no. of years worth of data to ignore due to spin-up
-file_suffix = '_interp'
-files = discard_spinup2(exp, time, file_suffix, years)
-ds = xr.open_mfdataset(files, decode_times=False)
-p = ds.coords['pfull'].data
-lat = ds.coords['lat'].data
-uz, div, ep1, ep2 = calc_ep(ds)
+def plot_single(ds, exp_name):
+    p = ds.coords['pfull'].data
+    lat = ds.coords['lat'].data
+    uz, div, ep1, ep2 = calc_ep(ds)
 
-#Filled contour plot of time-mean EP flux divergence plus EP flux arrows and zonal wind contours
-divlvls = 21 #np.arange(-2,2.1,0.1)
-ulvls = np.arange(-200, 200, 10)
-fig, ax = plt.subplots(figsize=(8,6))
-uz.plot.contour(colors='k', linewidths=0.5, alpha=0.4, levels=ulvls)
-div_scaled = div #(div.transpose()/p).transpose()
-cs = div_scaled.plot.contourf(levels=divlvls, cmap='RdBu_r', add_colorbar=False)
-cb = plt.colorbar(cs)
-#cb.set_label(label=r'Pressure-scaled Divergence (m s$^{-1}$ day$^{-1}$)', size='large')
-cb.set_label(label=r'Divergence (m s$^{-1}$ day$^{-1}$)', size='large')
-ticklabs = cb.ax.get_yticklabels()
-cb.ax.set_yticklabels(ticklabs, fontsize='large')
-climate.PlotEPfluxArrows(ds.lat,ds.pfull,ep1,ep2,fig,ax,yscale='log')
-plt.xlabel('Latitude', fontsize='x-large')
-plt.xlim(-90,90)
-plt.xticks([-90, -45, 0, 45, 90], ['90S', '45S', '0', '45N', '90N'])
-plt.ylabel('Pressure (hPa)', fontsize='x-large')
-plt.yscale('log')
-plt.ylim(max(p), 1) #to 1 hPa
-plt.tick_params(axis='both', labelsize = 'large', which='both', direction='in')
-plt.title('Time and Zonal Mean EP Flux', fontsize='x-large')
-plt.savefig(exp+'_EPflux.pdf', bbox_inches = 'tight')
-plt.close()
+    #Filled contour plot of time-mean EP flux divergence plus EP flux arrows and zonal wind contours
+    divlvls = np.arange(-12,13,1)
+    ulvls = np.arange(-200, 200, 10)
+    fig, ax = plt.subplots(figsize=(8,6))
+    uz.plot.contour(colors='k', linewidths=0.5, alpha=0.4, levels=ulvls)
+    cs = div.plot.contourf(levels=divlvls, cmap='RdBu_r', add_colorbar=False)
+    cb = plt.colorbar(cs)
+    cb.set_label(label=r'Divergence (m s$^{-1}$ day$^{-1}$)', size='large')
+    cb.ax.set_yticks(divlvls)
+    plt.draw()
+    ticklabs = cb.ax.get_yticklabels()
+    cb.ax.set_yticklabels(ticklabs, fontsize='large')
+    climate.PlotEPfluxArrows(ds.lat,ds.pfull,ep1,ep2,fig,ax,yscale='log')
+    plt.xlabel(r'Latitude ($\degree$N)', fontsize='x-large')
+    plt.xlim(0,90)
+    plt.xticks([0, 45, 90], ['0', '45', '90'])
+    plt.ylabel('Pressure (hPa)', fontsize='x-large')
+    plt.yscale('log')
+    plt.ylim(max(p), 1) #to 1 hPa
+    plt.tick_params(axis='both', labelsize = 'large', which='both', direction='in')
+    #plt.title('Time and Zonal Mean EP Flux', fontsize='x-large')
+    plt.savefig(exp+'_EPflux.pdf', bbox_inches = 'tight')
+    return plt.close()
+
+def plot_diff(ds1, ds2, exp):
+    p = ds1.coords['pfull'].data
+    lat = ds1.coords['lat'].data
+    uz, div1, ep1, ep2 = calc_ep(ds1)
+    uz, div2, ep1, ep2 = calc_ep(ds2)
+    div_diff = div2 - div1
+
+    #Filled contour plot of the difference in time-mean EP flux divergence
+    divlvls = np.arange(-12,13,1)
+    difflvls = np.arange(-2,2.1,0.1)
+    fig, ax = plt.subplots(figsize=(8,6))
+    cs1 = ax.contourf(lat, p, div_diff, levels=difflvls, cmap='RdBu_r')
+    ax.contourf(cs1, colors='none')
+    cs2 = ax.contour(lat, p, div1, colors='k', levels=divlvls, linewidths=1)
+    cb = plt.colorbar(cs1)
+    cb.set_label(label=r'Difference in Divergence (m s$^{-1}$ day$^{-1}$)', size='x-large')
+    cb.ax.set_yticks(difflvls)
+    plt.draw()
+    ticklabs = cb.ax.get_yticklabels()
+    cb.ax.set_yticklabels(ticklabs, fontsize='large')
+    plt.xlabel(r'Latitude ($\degree$N)', fontsize='x-large')
+    plt.xlim(0,90)
+    plt.xticks([0, 45, 90], ['0', '45', '90'])
+    plt.ylabel('Pressure (hPa)', fontsize='x-large')
+    plt.yscale('log')
+    plt.ylim(max(p), 1) #to 1 hPa
+    plt.tick_params(axis='both', labelsize = 'large', which='both', direction='in')
+    #plt.title('Difference in Time and Zonal Mean EP Flux Divergence', fontsize='x-large')
+    plt.savefig(exp+'_EPfluxdiff.pdf', bbox_inches = 'tight')
+    return plt.close()
+
+if __name__ == '__main__': 
+    #Set-up data to be read in
+    basis = 'PK_e0v4z13'
+    exp_name = basis+'_q6m2y45l800u200' #_w15a4p300f800g50_q6m2y45l800u200'
+    exp = [exp_name, basis]
+    time = 'daily'
+    file_suffix = '_interp'
+    years = 2 # user sets no. of years worth of data to ignore due to spin-up
+    
+    plot_type = input("Plot a) single experiment or b) difference between 2 experiments?")
+    if plot_type =='a':
+        ds = discard_spinup1(exp[0], time, file_suffix, years)
+        plot_single(ds, exp_name)
+    elif plot_type == 'b':
+        ds1 = discard_spinup1(exp[0], time, file_suffix, years)
+        ds2 = discard_spinup1(exp[1], time, file_suffix, years)
+        plot_diff(ds1, ds2, exp_name)
