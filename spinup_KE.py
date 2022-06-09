02f4184aee98d97e7bd0a59fffcc1dc7594c70f7
Regan Mudhar <rm811@emps-gv3.ex.ac.uk>
2022-06-09 15:03:09 +0100

    rewritten to be called by postprocessing script

--- spinup_KE.py
+++ spinup_KE.py
@@ -5,7 +5,7 @@
     Then take a weighted average along lat, and finally a mean along pressure.
     Based on script by Penelope Maher and discussions with William Seviour.
 """
-
+import sys
 from glob import glob
 import xarray as xr
 import numpy as np
@@ -15,38 +15,38 @@ def calc_TKE(u,v):
     upv = u*u + v*v
     return 0.5 * upv
 
-path = '/disco/share/rm811/isca_data/' #'../isca_data/'
-exp_name = 'PK_e0v4z13_w15a4p600f800g50_q6m2y45l800u200'
-files=sorted(glob(path+exp_name+'/run*/atmos_daily.nc'))
-iter = np.arange(0,len(files))
+path = str(sys.argv[1]) 
+exp_name = str(sys.argv[2])
+files=sorted(glob(path+'/'+exp_name+'/run*/*.nc'))
 KE = []
 print("finding KE")
-for i in iter:
-    #print(i)
+for i in range(len(files)):
+    print(i)
     file  = files[i]
     ds = xr.open_dataset(file, decode_times=False)
-    lat = ds.coords['lat'].data
-    lon = ds.coords['lon'].data
-    p = ds.coords['pfull'].data
-    u = ds.ucomp.mean(dim='lon').mean(dim='time')
-    v = ds.vcomp.mean(dim='lon').mean(dim='time')
+    u = ds.ucomp.mean(dim='lon')
+    v = ds.vcomp.mean(dim='lon')
     coslat = np.cos(np.deg2rad(u.coords['lat'].values)).clip(0., 1.) # need to weight due to different box sizes over grid
     lat_wgts = np.sqrt(coslat)
-    TKE_box = np.empty_like(u)
-    for q in range(len(p)):
-        for j in range(len(lat)):
-            TKE_box[q,j] = calc_TKE(u[q,j], v[q,j])
-    
-    TKE_box = np.average(TKE_box, axis=1, weights=lat_wgts)
-    TKE_avg = np.nanmean(TKE_box) # should I weight pressures too? How?
-    KE.append(TKE_avg)
+    for j in range(len(u)):
+        print(j)
+        TKE_box = np.empty_like(u[j])
+        for q in range(len(ds.coords['pfull'].data)):
+            for k in range(len(ds.coords['lat'].data)):
+                TKE_box[q,k] = calc_TKE(u[j][q,k], v[j][q,k])
+        TKE_box = np.average(TKE_box, axis=1, weights=lat_wgts)
+        TKE_avg = np.nanmean(TKE_box) # should I weight pressures too? How?
+        KE.append(TKE_avg)
+save_file(exp_name, KE, 'KE')
 
-print("plotting")
-fig, ax = plt.subplots(figsize=(11,6))
-ax.plot(iter+1, KE, color='k')
-ax.set_xlim(1,len(files))
-ax.set_xlabel("Run no.")       
-ax.set_ylabel('TKE', color='k')
-plt.title("Total Kinetic Energy for "+exp_name) #~{0:.0f}y worth".format(len(files)/12))
-plt.savefig(exp_name+'_spinup.pdf', bbox_inches = 'tight')
-plt.close()
+# option to plot the KE over time
+#print("plotting")
+#KE = open_file(exp_name, 'KE')
+#fig, ax = plt.subplots(figsize=(10,6))
+#ax.plot(len(KE), KE, color='k')
+#ax.set_xlim(1,len(KE))
+#ax.set_xlabel("Days Simulated")       
+#ax.set_ylabel('TKE', color='k')
+#plt.title("Total Kinetic Energy for "+exp_name)
+#plt.savefig(exp_name+'_spinup.pdf', bbox_inches = 'tight')
+#plt.close()
