a864fed352871dcbb2cb3f5a6c3de14f1c717585
Regan Mudhar <rm811@emps-gv3.ex.ac.uk>
2022-06-21 14:39:53 +0100

    rewritten to use processed files

--- EOFs.py
+++ EOFs.py
@@ -13,38 +13,36 @@ from eofs.xarray import Eof
 import statsmodels.api as sm
 from shared_functions import *
 
-def eof_u(ds, p_min, lat_min):
+def eof_u(uz, p_min, lat_min):
     """
     Follows Sheshadri & Plumb 2017, uses p>100hPa, lat>20degN and zonal mean zonal wind.
     """
-    uz = ds.ucomp\
-        .sel(pfull=slice(p_min,1000))\
-        .sel(lat=slice(lat_min,90))\
-        .mean(dim='lon')
-    return uz
+    u_new = uz.sel(pfull=slice(p_min,1000)).sel(lat=slice(lat_min,90))
 
-def eof_solver(ds, p_min, lat_min):
+    return u_new
+
+def eof_solver(uz, p_min, lat_min):
     """
     Finds the zonal wind anomaly and latitude/pressure weightings.
     Returns the 'solver' for further EOF analyses.
     """
-    uz = eof_u(ds, p_min, lat_min)
+    u = eof_u(uz, p_min, lat_min)
 
     # Calculate anomalies
-    uz_anom = uz - uz.mean(dim='time')
+    u_anom = u - u.mean(dim='time')
 
     # sqrt(cos(lat)) weights due to different box sizes over grid
-    sqrtcoslat = np.sqrt(np.cos(np.deg2rad(uz_anom.coords['lat'].values))) 
+    sqrtcoslat = np.sqrt(np.cos(np.deg2rad(u_anom.coords['lat'].values))) 
 
     # sqrt(dp) weights, select correct number of levels
-    nplevs = uz_anom.coords['pfull'].shape[0]
+    nplevs = u_anom.coords['pfull'].shape[0]
     sqrtdp = np.sqrt(np.diff(ds.coords['phalf'].values[-nplevs-2:-1]))
 
     # Calculate gridpoint weights
     wgts = np.outer(sqrtdp,sqrtcoslat)
 
     # Create an EOF solver to do the EOF analysis.
-    solver = Eof(uz_anom.compute(), weights=wgts)
+    solver = Eof(u_anom.compute(), weights=wgts)
 
     return solver
 
@@ -96,13 +94,13 @@ def AM_times(pcs, lags):
 
     return tau1, tau2
 
-def plot_single(ds, p_min, lat_min, exp_name):
+def plot_single(uz, utz, p_min, lat_min, exp_name):
     """
     Plots equivalent to Fig 4a-c from Sheshadri & Plumb.
     """
     # First generate all necessary information for EOF analysis
-    uz = eof_u(ds, p_min, lat_min)
-    solver = eof_solver(ds, p_min, lat_min)
+    u = eof_u(uz, p_min, lat_min)
+    solver = eof_solver(uz, p_min, lat_min)
     eofs = leading_eofs(solver)
     pcs = leading_pcs(solver)
     variance_fractions = variance(solver)
@@ -110,11 +108,10 @@ def plot_single(ds, p_min, lat_min, exp_name):
     tau1, tau2 = AM_times(pcs, lags)
 
     # Set-up coordinates
-    lat = ds.coords['lat'].sel(lat=slice(lat_min,90))
-    p = ds.coords['pfull'].sel(pfull=slice(p_min,1000))
+    lat = uz.coords['lat'].sel(lat=slice(lat_min,90))
+    p = uz.coords['pfull'].sel(pfull=slice(p_min,1000))
 
     # Set-up variables
-    uz = uz.mean(dim='time')
     eof1 = eofs.sel(mode=0)
     eof2 = eofs.sel(mode=1)
     
@@ -126,12 +123,13 @@ def plot_single(ds, p_min, lat_min, exp_name):
     ax1 = fig.add_subplot(1,3,1)
     eof_1 = eof1.plot.contourf(ax=ax1, cmap='RdBu_r', yincrease=False, levels=21, add_colorbar=False)
     plt.colorbar(eof_1)
-    uz.plot.contour(ax=ax1, colors='k', yincrease=False, levels=ulvls)
-    ax1.set_ylabel('Pressure (hPa)', fontsize='large')
+    utz.plot.contour(ax=ax1, colors='k', yincrease=False, levels=ulvls)
+    ax1.set_xlim(lat_min,90)
+    ax1.set_ylabel('Pressure (hPa)', fontsize='x-large')
     ax1.set_ylim(925,100)
 
-    ax1.set_xlabel(r'Latitude ($\degree$N)', fontsize='large')
-    ax1.tick_params(axis='both', labelsize = 'medium', which='both', direction='in')
+    ax1.set_xlabel(r'Latitude ($\degree$N)', fontsize='x-large')
+    ax1.tick_params(axis='both', labelsize = 'large', which='both', direction='in')
     ax1.set_title(r'EOF1, {0:.0f}% of variance, $\tau$ = {1:.0f} days'\
         .format(100*variance_fractions.values[0],\
         tau1), fontsize='x-large')
@@ -139,12 +137,13 @@ def plot_single(ds, p_min, lat_min, exp_name):
     ax2 = fig.add_subplot(1,3,2)
     eof_2 = eof2.plot.contourf(ax=ax2, cmap='RdBu_r', yincrease=False, levels=21, add_colorbar=False)
     plt.colorbar(eof_2)
-    uz.plot.contour(ax=ax2, colors='k', yincrease=False, levels=ulvls)
-    ax2.set_ylabel('Pressure (hPa)', fontsize='large')
+    utz.plot.contour(ax=ax2, colors='k', yincrease=False, levels=ulvls)
+    ax2.set_xlim(lat_min,90)
+    ax2.set_ylabel('Pressure (hPa)', fontsize='x-large')
     ax2.set_ylim(925,100)
     
-    ax2.set_xlabel(r'Latitude ($\degree$N)', fontsize='large')
-    ax2.tick_params(axis='both', labelsize = 'medium', which='both', direction='in')
+    ax2.set_xlabel(r'Latitude ($\degree$N)', fontsize='x-large')
+    ax2.tick_params(axis='both', labelsize = 'large', which='both', direction='in')
     ax2.set_title(r'EOF2, {0:.0f}% of variance, $\tau$ = {1:.0f} days'\
         .format(100*variance_fractions.values[1],\
         tau2), fontsize='x-large')
@@ -157,34 +156,23 @@ def plot_single(ds, p_min, lat_min, exp_name):
     ax3.axvline(0, color='k', linewidth=0.5)
     ax3.axhline(1/np.e, color='#D2D0D3', linestyle=":")
     ax3.set_xlim(-1*lags, lags)
-    ax3.set_xlabel('Lag (days)', fontsize='large')
+    ax3.set_xlabel('Lag (days)', fontsize='x-large')
     ax3.set_ylim(-0.2,1)
-    ax3.tick_params(axis='both', labelsize = 'medium', which='both', direction='in')
+    ax3.tick_params(axis='both', labelsize = 'large', which='both', direction='in')
     ax3.set_title('PC autocorrelation', fontsize='x-large')
-
-
     plt.savefig(exp_name+'_eofs.pdf', bbox_inches = 'tight')
-    plt.close()
 
-    return plt.show()
+    return plt.close()
 
-def plot_multi(ds1, ds2, ds3, ds4, p_min, lat_min, labels, colors, style, cols, fig_name):
+def plot_multi(uz, lags, p_min, lat_min, labels, colors, style, cols, fig_name):
     """
     Plots equivalent to Fig 4c from Sheshadri & Plumb to compare experiments.
     """
-    # First generate all necessary information for EOF analysis
-    pc1 = leading_pcs(eof_solver(ds1, p_min, lat_min))
-    pc2 = leading_pcs(eof_solver(ds2, p_min, lat_min))
-    pc3 = leading_pcs(eof_solver(ds3, p_min, lat_min))
-    pc4 = leading_pcs(eof_solver(ds4, p_min, lat_min))
-    lags = 100 
-    
-    # Now plot on a single figure
+    # Plot on a single figure
     fig = plt.figure(figsize=(10,8))
-    plt.acorr(pc1.sel(mode=0), maxlags=lags, usevlines=False, color=colors[0], linestyle=style[0], marker=None, linewidth=1, label=labels[0])
-    plt.acorr(pc2.sel(mode=0), maxlags=lags, usevlines=False, color=colors[1], linestyle=style[1], marker=None, linewidth=1, label=labels[1])
-    plt.acorr(pc3.sel(mode=0), maxlags=lags, usevlines=False, color=colors[2], linestyle=style[2], marker=None, linewidth=1, label=labels[2])
-    plt.acorr(pc4.sel(mode=0), maxlags=lags, usevlines=False, color=colors[3], linestyle=style[3], marker=None, linewidth=1, label=labels[3])
+    for i in range(len(uz)):
+        pc = leading_pcs(eof_solver(uz[i], p_min, lat_min))
+        plt.acorr(pc.sel(mode=0), maxlags=lags, usevlines=False, color=colors[i], linestyle=style[i], marker=None, linewidth=1, label=labels[i])
     plt.legend(loc='upper center' , bbox_to_anchor=(0.5, -0.07), fancybox=False, shadow=True, ncol=cols, fontsize='large')
     plt.axhline(0, color='k', linewidth=0.5)
     plt.axvline(0, color='k', linewidth=0.5)
@@ -195,48 +183,35 @@ def plot_multi(ds1, ds2, ds3, ds4, p_min, lat_min, labels, colors, style, cols, 
     plt.tick_params(axis='both', labelsize = 'large', which='both', direction='in')
     plt.title('EOF1 PC autocorrelation', fontsize='x-large')
     plt.savefig(fig_name+'_pcs.pdf', bbox_inches = 'tight')
-    plt.close()
 
-    return plt.show()
+    return plt.close()
 
 if __name__ == '__main__': 
     plot_type = input("a) single or b) multi?")
 
-    time = 'daily'
-    years = 2 # user sets no. of years worth of data to ignore due to spin-up
-    file_suffix = '_interp'    
+    indir = '/disco/share/rm811/processed/'
+    basis = 'PK_e0v4z13'
+    exp = ['test', 'test'] #[basis, basis+'_q6m2y45l800u200']
 
     # For EOFs follow Sheshadri & Plumb 2017, use p>100hPa, lat>20degN
     p_min = 100  # hPa
     lat_min = 20  # degrees
 
     if plot_type=='a':
-        exp_name = 'PK_e0v4z13_w40a4p800f800g50_q6m2y45l800u200'
-        ds = add_phalf(exp_name, time, file_suffix, years)
-        print(exp_name)
-        plot_single(ds, p_min, lat_min, exp_name, alt=False)
+        ds = add_phalf(indir+exp[0], '_zmean.nc')
+        uz = ds.ucomp
+        utz = xr.open_dataset(indir+exp[0]+'_tzmean.nc', decode_times=False).ucomp[0]
+        plot_single(uz, utz, p_min, lat_min, exp[0])
     
     elif plot_type=='b':
-        basis = 'PK_eps0_vtx1_zoz13'
-        exp = [basis+'_7y',\
-            basis+'_w15a4p800f800g50',\
-            basis+'_w15a4p600f800g50',\
-            basis+'_w15a4p400f800g50']
+        uz = []
+        for i in range(len(exp)):
+            ds = add_phalf(indir+exp[i], '_zmean.nc')
+            uz.append(ds.ucomp)
 
-        ds1 = add_phalf(exp[0], time, file_suffix, years)
-        ds2 = add_phalf(exp[1], time, file_suffix, years)
-        ds3 = add_phalf(exp[2], time, file_suffix, years)
-        ds4 = add_phalf(exp[3], time, file_suffix, years)
+        labels = ['label1', 'label2']
+        colors = ['#B30000', '#FF9900']
+        style = ['-', ':']
+        cols = len(exp)
 
-        #labels = [r'$\gamma$ = 1',r'$\gamma$ = 2',r'$\gamma$ = 3',r'$\gamma$ = 4']
-        colors = ['k', '#C0392B', '#27AE60', '#9B59B6']
-        style = ['-', '-', '-', '-']
-        cols = 4
-        #labels = [r'$\epsilon = 0, p_{trop} \sim 100$ hPa', r'$\epsilon = 10, p_{trop} \sim 100$ hPa', r'$\epsilon = 0, p_{trop} \sim 200$ hPa', r'$\epsilon = 10, p_{trop} \sim 200$ hPa']
-        #colors = ['#2980B9', '#2980B9', 'k', 'k']
-        #style = ['--', '-', '--', '-']
-        #cols = 2
-        #labels = ['no heat', r'A = 2 K day$^{-1}$', r'A = 4 K day$^{-1}$', r'A = 8 K day$^{-1}$']
-        labels = ['no heat', r'p$_{top}$ = 800 hPa', r'p$_{top}$ = 600 hPa', r'p$_{top}$ = 400 hPa']
-
-        plot_multi(ds1, ds2, ds3, ds4, p_min, lat_min, labels, colors, style, cols, 'PK_eps0_vtx1_zoz13_heating3')        
+        plot_multi(uz, 75, p_min, lat_min, labels, colors, style, cols, exp[0])    
