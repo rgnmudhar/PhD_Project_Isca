1731a9d93f113c559ad087a4c28bbb97a9db3429
Regan Mudhar <rm811@emps-gv1.ex.ac.uk>
2022-08-31 16:05:56 +0100

    latest analysis scripts

--- heatflux.py
+++ heatflux.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 from shared_functions import *
 
-def find_anom(exp):
+def find_anom_all(exp):
     print(datetime.now(), " - opening files")
     v = xr.open_dataset(indir+exp+'_v.nc', decode_times=False).vcomp
     T = xr.open_dataset(indir+exp+'_T.nc', decode_times=False).temp
@@ -21,13 +21,30 @@ def find_anom(exp):
     vpTp_bar = (vp*Tp).mean('lon')
     return vpTp_bar
 
+def find_anom_p(exp, p):
+    # Meridional heat flux weighted by cos(lat) and meridionally averaged from 60 to 90 N at p hPa
+    # Based on Dunn-Sigouin & Shaw (2015)
+    print(datetime.now(), " - opening files for ", exp)
+    v = xr.open_dataset(indir+exp+'_v.nc', decode_times=False).vcomp.sel(pfull=p, method='nearest')
+    T = xr.open_dataset(indir+exp+'_T.nc', decode_times=False).temp.sel(pfull=p, method='nearest')
+    vz = xr.open_dataset(indir+exp+'_vz.nc', decode_times=False).vcomp.sel(pfull=p, method='nearest')
+    Tz = xr.open_dataset(indir+exp+'_Tz.nc', decode_times=False).temp.sel(pfull=p, method='nearest')
+    print(datetime.now(), " - finding anomalies")
+    vp = v - vz
+    Tp = T - Tz
+    print(datetime.now(), " - finding v'T'")
+    vpTp_w = (vp*Tp) / np.cos(np.deg2rad(vz.lat))
+    vpTp_sub = vpTp_w.sel(lat=slice(60,90)).mean('lat')
+    vpTp_bar = vpTp_sub.mean('lon')
+    return vpTp_bar
+
 def plot(vpTp, exp, heat, lvls, colors):
     print(datetime.now(), " - plotting ", exp)
     fig, ax = plt.subplots(figsize=(6,6))
     cs = ax.contourf(lat, p, vpTp.mean('time'), levels=lvls, cmap=colors)
     ax.contourf(cs, colors='none')
     cb = plt.colorbar(cs)
-    cb.set_label(label=r"v'T' (m s$^{-1}$ K)", size='x-large')
+    cb.set_label(label=r"v'T' (K m s$^{-1}$)", size='x-large')
     cb.ax.tick_params(labelsize='x-large')
     plt.contour(lat, p, heat, colors='g', linewidths=1, alpha=0.4, levels=11)
     plt.xlabel(r'Latitude ($\degree$N)', fontsize='x-large')
@@ -85,71 +102,76 @@ if __name__ == '__main__':
             basis+'_a4x75y270w5v30p800'+perturb]
         labels = [r'no heat', '0', '90', '180', '270']
         xlabel = r'Longitude of Heating ($\degree$E)'
-    labels = ['no heat', '900', '800', '700', '600', '500', '400', '300']
-    colors = ['#B30000', '#FF9900', '#FFCC00', '#00B300', '#0099CC', '#4D0099', '#CC0080', '#666666']
-    levels = [500, 100, 50, 10]
-
-    vpTp = []
-    for i in range(len(exp)):
-        vpTp.append(find_anom(exp[i]))
-        file = '/disco/share/rm811/isca_data/' + exp[i]+ '/run0100/atmos_daily_interp.nc'
-        ds = xr.open_dataset(file)
-        heat = ds.local_heating.sel(lon=180, method='nearest').mean(dim='time')
-        lat = ds.coords['lat'].data
-        p = ds.coords['pfull'].data
-        plot(vpTp[i], exp[i], heat, np.arange(-20, 190, 10), 'Blues')
-
-        if i != 0:
-            vpTp_diff = vpTp[i] - vpTp[0]
-            plot(vpTp_diff, exp[i]+'_diff', heat, np.arange(-40, 42, 2), 'RdBu_r')   
-
-    mode = []
-    sd = []
-    for j in range(len(levels)):
-        print(datetime.now(), " - plotting v'T' PDFs at {:.0f} hPa".format(levels[j]))
-        mode_sub = []
-        sd_sub = []
-        x_min = x_max = 0 
-        fig, ax = plt.subplots(figsize=(6,6))
-        for i in range(len(exp)):
-            x, f, m = pdf(vpTp[i].sel(pfull=levels[j], method='nearest').sel(lat=60, method='nearest'))
-            mode_sub.append(m)
-            sd_sub.append(np.std(x))
-            if max(x) > x_max:
-                x_max = max(x)
-            if min(x) < x_min:
-                x_min = min(x)
-            ax.plot(x, f, linewidth=1.25, color=colors[i], label=labels[i])
-        mode.append(mode_sub)
-        sd.append(sd_sub)
-        ax.set_xlim(x_min, x_max)
-        ax.set_xlabel(r"v'T' (m s$^{-1}$ K)", fontsize='x-large')
-        ax.tick_params(axis='both', labelsize = 'x-large', which='both', direction='in')
-        plt.legend(labels=labels, loc='upper right',fancybox=False, shadow=True, ncol=1, fontsize='large')
-        plt.savefig(exp[0]+extension+'_vpTp'+'_{:.0f}pdf.pdf'.format(levels[j]), bbox_inches = 'tight')
-        plt.close()
     
-    print(datetime.now(), " - plotting v'T' mode")
-    colors = ['#B30000', '#00B300', '#0099CC', 'k']
-    fig, ax = plt.subplots(figsize=(8,6))
-    for j in range(len(levels)):
-        ax.plot(labels, mode[j], marker='o', linewidth=1.25, color=colors[j], linestyle=':', label='{:.0f} hPa'.format(levels[j]))
-    ax.set_xticks(labels)
-    ax.set_xlabel(xlabel, fontsize='x-large')
-    ax.set_ylabel(r" 60 N v'T' mode (m s$^{-1}$ K)", fontsize='x-large')
-    ax.tick_params(axis='both', labelsize = 'x-large', which='both', direction='in')
-    plt.legend(loc='right',fancybox=False, shadow=True, ncol=1, fontsize='large')
-    plt.savefig(exp[0]+extension+'_vpTp'+'_mode.pdf', bbox_inches = 'tight')
-    plt.close()
+    colors = ['#B30000', '#FF9900', '#FFCC00', '#00B300', '#0099CC', '#4D0099', '#CC0080', '#666666']
 
-    print(datetime.now(), " - plotting v'T' s.d.")
-    fig, ax = plt.subplots(figsize=(8,6))
-    for j in range(len(levels)):
-        ax.plot(labels, sd[j], marker='o', linewidth=1.25, color=colors[j], linestyle=':', label='{:.0f} hPa'.format(levels[j]))
-    ax.set_xticks(labels)
-    ax.set_xlabel(xlabel, fontsize='x-large')
-    ax.set_ylabel(r" 60 N v'T' S.D. (m s$^{-1}$ K)", fontsize='x-large')
-    ax.tick_params(axis='both', labelsize = 'x-large', which='both', direction='in')
-    plt.legend(loc='upper right',fancybox=False, shadow=True, ncol=1, fontsize='large')
-    plt.savefig(exp[0]+extension+'_vpTp'+'_sd.pdf', bbox_inches = 'tight')
-    plt.close()
+    plot_option = input("Plot a) lat-p of v'T' or b) PDFs of polar cap average v'T'?")
+
+    if plot_option == 'a':
+        vpTp = []
+        for i in range(len(exp)):
+            vpTp.append(find_anom_all(exp[i]))
+            file = '/disco/share/rm811/isca_data/' + exp[i]+ '/run0100/atmos_daily_interp.nc'
+            ds = xr.open_dataset(file)
+            heat = ds.local_heating.sel(lon=180, method='nearest').mean(dim='time')
+            lat = ds.coords['lat'].data
+            p = ds.coords['pfull'].data
+            plot(vpTp[i], exp[i], heat, np.arange(-20, 190, 10), 'Blues')
+
+            if i != 0:
+                vpTp_diff = vpTp[i] - vpTp[0]
+                plot(vpTp_diff, exp[i]+'_diff', heat, np.arange(-60, 65, 5), 'RdBu_r')   
+    
+    elif plot_option == 'b':
+        p = [500, 100, 50, 10]
+        mode = []
+        sd = []
+        for j in range(len(p)):
+            print(datetime.now(), ' - {:.0f} hPa'.format(p[j]))
+            x_min = x_max = 0
+            sub_mode = []
+            sub_sd = []
+            fig, ax = plt.subplots(figsize=(6,6))
+            for i in range(len(exp)):
+                x, f, m = pdf(find_anom_p(exp[i], p[j]))
+                sub_mode.append(m)
+                sub_sd.append(np.std(x))
+                if max(x) > x_max:
+                    x_max = max(x)
+                if min(x) < x_min:
+                    x_min = min(x)
+                print(datetime.now(), ' - plotting')
+                ax.plot(x, f, linewidth=1.25, color=colors[i], label=labels[i])
+            mode.append(sub_mode)
+            sd.append(sub_sd)
+            ax.set_xlim(x_min, x_max)
+            ax.set_xlabel(r"Polar Cap Average v'T' (K m s$^{-1}$)", fontsize='x-large')
+            ax.tick_params(axis='both', labelsize = 'x-large', which='both', direction='in')
+            plt.legend(labels=labels, loc='upper right',fancybox=False, shadow=True, ncol=1, fontsize='large')
+            plt.savefig(exp[0]+extension+'_vpTp'+'_{:.0f}pdf.pdf'.format(p[j]), bbox_inches = 'tight')
+            plt.close()
+        
+        print(datetime.now(), " - plotting v'T' mode")
+        colors = ['#B30000', '#00B300', '#0099CC', 'k']
+        fig, ax = plt.subplots(figsize=(8,6))
+        for j in range(len(p)):
+            ax.plot(labels, mode[j], marker='o', linewidth=1.25, color=colors[j], linestyle=':', label='{:.0f} hPa'.format(p[j]))
+        ax.set_xticks(labels)
+        ax.set_xlabel(xlabel, fontsize='x-large')
+        ax.set_ylabel(r"Polar Cap Average v'T' mode (K m s$^{-1}$)", fontsize='x-large')
+        ax.tick_params(axis='both', labelsize = 'x-large', which='both', direction='in')
+        plt.legend(loc='right',fancybox=False, shadow=True, ncol=1, fontsize='large')
+        plt.savefig(exp[0]+extension+'_vpTp'+'_mode.pdf', bbox_inches = 'tight')
+        plt.close()
+
+        print(datetime.now(), " - plotting v'T' s.d.")
+        fig, ax = plt.subplots(figsize=(8,6))
+        for j in range(len(p)):
+            ax.plot(labels, sd[j], marker='o', linewidth=1.25, color=colors[j], linestyle=':', label='{:.0f} hPa'.format(p[j]))
+        ax.set_xticks(labels)
+        ax.set_xlabel(xlabel, fontsize='x-large')
+        ax.set_ylabel(r"Polar Cap Average v'T' S.D. (K m s$^{-1}$)", fontsize='x-large')
+        ax.tick_params(axis='both', labelsize = 'x-large', which='both', direction='in')
+        plt.legend(loc='right',fancybox=False, shadow=True, ncol=1, fontsize='large')
+        plt.savefig(exp[0]+extension+'_vpTp'+'_sd.pdf', bbox_inches = 'tight')
+        plt.close()
--- stationary_wave.py
+++ stationary_wave.py
@@ -74,9 +74,8 @@ if __name__ == '__main__':
     basis = 'PK_e0v4z13'
     perturb = '_q6m2y45'
     off_pole = '_a4x75y180w5v30p800'
-    ctrl = basis +'_q6m2y45l800u200'
-    exp = [basis+'_a4x75y180w5v30p400'+perturb,
-        basis+off_pole+perturb]
+    polar = '_w15a4p800f800g50'
+    exp = [basis+off_pole, basis+'_q6m2y45l800u200', basis+off_pole+perturb]
     lons = [0, 90, 180, 270]
     p = [850, 200, 10]
     p_lvls_diff = [np.arange(-150, 170, 20), np.arange(-200, 220, 20), np.arange(-250, 270, 20)]
@@ -122,10 +121,6 @@ if __name__ == '__main__':
                 plot_v(gph_tz[0], 21, heat, exp[i]+'_gph_v.pdf', 'GPH (m)')
 """
 
-#exp = [ctrl,\
-#    basis+off_pole,\
-#    basis+off_pole+perturb] 
-
 for j in range(len(p)):
     print(datetime.now(), " - p = {0:.0f} hPa".format(p[j]))
     anomalies = []
@@ -133,12 +128,21 @@ for j in range(len(p)):
         gph_t = xr.open_dataset(indir+exp[i]+'_ht.nc', decode_times=False).height.sel(pfull=p[j], method='nearest')
         gph_tz = xr.open_dataset(indir+exp[i]+'_htz.nc', decode_times=False).height.sel(pfull=p[j], method='nearest')
         anomalies.append(anomaly(gph_t, gph_tz)[0])
-#    anom_add = anomalies[0] + anomalies[1]
-#    anom_combo = anomalies[2]
-#    anom_diff = anom_combo - anom_add
-    ds, heat1 = find_heat(exp[0], 500, type='exp')
-    ds, heat2 = find_heat(exp[1], 1000, type='polar')
-    anom_diff = anomalies[0] - anomalies[1]
+    print(datetime.now(), " - addition")
+    anom_add = anomalies[0] + anomalies[1]
+    print(datetime.now(), " - combo")
+    anom_combo = anomalies[2]
+    print(datetime.now(), " - difference")
+    anom_diff = anom_combo - anom_add
+    ds, heat1 = find_heat(exp[0], 1000, type='polar')
+    ds, heat2 = find_heat(exp[1], 500, type='exp')
+    #anom_diff = anomalies[0] - anomalies[1]
+    plotting = [anomalies[0], anomalies[1], anom_add, anomalies[2], anom_diff]
+    names = [exp[0], exp[1], exp[0]+'+ctrl', exp[2], 'combo-'+exp[0]+'+ctrl']
+    lvls_full = [np.arange(-120, 130, 10), np.arange(-200, 220, 20), np.arange(-250, 270, 20)]
+    lvls_diff = [np.arange(-25, 27, 2), np.arange(-60, 70, 10), np.arange(-150, 170, 20)]
+    lvls_polar = [np.arange(-10, 11, 1), np.arange(-16, 18, 2), np.arange(-30, 35, 5)] #np.arange(-7, 8, 1)
+    lvls = [lvls_polar[j], lvls_full[j], lvls_full[j], lvls_full[j], lvls_diff[j]]
 
 #    print(datetime.now(), " - plotting")
 #    ax = plt.axes(projection=ccrs.NorthPolarStereo())
@@ -157,19 +161,21 @@ for j in range(len(p)):
 #    plt.savefig(exp[1]+'+ctrl_'+str(p[j])+'gph.pdf', bbox_inches = 'tight')
 #    plt.close()
 
-    ax = plt.axes(projection=ccrs.NorthPolarStereo())
-    cs = ax.contourf(ds.coords['lon'].data, ds.coords['lat'].data, anom_diff,\
-    cmap='RdBu_r', levels=p_lvls_diff2[j], transform = ccrs.PlateCarree())
-    cb = plt.colorbar(cs, pad=0.1)
-    cb.set_label(label='Geopotential Height Anomaly Difference (m)', size='x-large')
-    cb.ax.tick_params(labelsize='x-large')
-    ln_ctrl = ax.contour(ds.coords['lon'].data, ds.coords['lat'].data, heat1,\
-        levels=11, colors='g', linewidths=0.5, alpha=0.4, transform = ccrs.PlateCarree())
-    ln_exp = ax.contour(ds.coords['lon'].data, ds.coords['lat'].data, heat2,\
-        levels=11, colors='g', linewidths=0.5, alpha=0.4, transform = ccrs.PlateCarree())
-    ax.set_global()
-    ax.gridlines(draw_labels=True, dms=True, x_inline=False, y_inline=False)
-    ax.set_extent([-180, 180, 0, 90], crs=ccrs.PlateCarree())
-    #plt.savefig('combo-'+exp[1]+'+ctrl_'+str(p[j])+'gph.pdf', bbox_inches = 'tight')
-    plt.savefig(basis+'_a4x75y180w5v30p400-800'+perturb+str(p[j])+'gph.pdf', bbox_inches = 'tight')
-    plt.close()
+    for k in range(len(plotting)):
+        print(datetime.now(), " - plotting ", names[k])
+        ax = plt.axes(projection=ccrs.NorthPolarStereo())
+        cs = ax.contourf(ds.coords['lon'].data, ds.coords['lat'].data, plotting[k],\
+        cmap='RdBu_r', levels=lvls[k], transform = ccrs.PlateCarree())
+        cb = plt.colorbar(cs, pad=0.1)
+        cb.set_label(label='Geopotential Height Anomaly (m)', size='x-large')
+        cb.ax.tick_params(labelsize='x-large')
+        ln_ctrl = ax.contour(ds.coords['lon'].data, ds.coords['lat'].data, heat1,\
+            levels=11, colors='g', linewidths=0.5, alpha=0.4, transform = ccrs.PlateCarree())
+        if k in range(1,5):
+            ln_exp = ax.contour(ds.coords['lon'].data, ds.coords['lat'].data, heat2,\
+                levels=11, colors='g', linewidths=0.5, alpha=0.4, transform = ccrs.PlateCarree())
+        ax.set_global()
+        ax.gridlines(draw_labels=True, dms=True, x_inline=False, y_inline=False)
+        ax.set_extent([-180, 180, 0, 90], crs=ccrs.PlateCarree())
+        plt.savefig(names[k]+'_'+str(p[j])+'gph.pdf', bbox_inches = 'tight')
+        plt.close()
