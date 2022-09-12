bd2619884dcbbb4248691b7e0e82f6e4a4b1cb13
Regan Mudhar <rm811@emps-gv3.ex.ac.uk>
2022-09-12 09:26:22 +0100

    option selection andplot/remove spinup functions edited

--- postprocessing.py
+++ postprocessing.py
@@ -5,6 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 import sys
+import shutil
 from shared_functions import *
 from datetime import datetime
 
@@ -58,7 +59,7 @@ def calc_TKE(u,v):
     upv = u*u + v*v
     return 0.5 * upv
 
-def find_TKE(dir, exp):
+def find_TKE(indir, outdir):
     """
     This attempts to use TKE for determining spin-up.
     For every 'time'  (i.e. atmos_monthly file) take the zonal mean of u and v along longitude.
@@ -66,31 +67,34 @@ def find_TKE(dir, exp):
     Then take a weighted average along lat, and finally a mean along pressure.
     Based on script by Penelope Maher and discussions with William Seviour.
     """
-    KE = []
-    print("finding KE")
-    uz = xr.open_dataset(dir+exp+'_uz.nc', decode_times=False).ucomp[0]
-    vz = xr.open_dataset(dir+exp+'_vz.nc', decode_times=False).vcomp[0]
-    coslat = np.cos(np.deg2rad(uz.coords['lat'].values)).clip(0., 1.) # need to weight due to different box sizes over grid
-    lat_wgts = np.sqrt(coslat)
-    for j in range(len(uz)):
-        TKE_box = np.empty_like(uz[j])
-        for q in range(len(uz.coords['pfull'].data)):
-            for k in range(len(uz.coords['lat'].data)):
-                TKE_box[q,k] = calc_TKE(uz[j][q,k], vz[j][q,k])
-        TKE_box = np.average(TKE_box, axis=1, weights=lat_wgts)
-        TKE_avg = np.nanmean(TKE_box) # should I weight pressures too? How?
-        KE.append(TKE_avg)
-    save_file(exp, KE, 'KE')
+    os.chdir(indir)
+    exp = sorted(glob('*')
+    for i in range(len(exp)):
+        print(datetime.now(), ' - finding KE for {0:.0f}/{1:.0f} - '.format(i+1, len(exp)), exp[i])
+        KE = []
+        uz = xr.open_dataset(dir+exp+'_uz.nc', decode_times=False).ucomp[0]
+        vz = xr.open_dataset(dir+exp+'_vz.nc', decode_times=False).vcomp[0]
+        coslat = np.cos(np.deg2rad(uz.coords['lat'].values)).clip(0., 1.) # need to weight due to different box sizes over grid
+        lat_wgts = np.sqrt(coslat)
+        for j in range(len(uz)):
+            TKE_box = np.empty_like(uz[j])
+            for q in range(len(uz.coords['pfull'].data)):
+                for k in range(len(uz.coords['lat'].data)):
+                    TKE_box[q,k] = calc_TKE(uz[j][q,k], vz[j][q,k])
+            TKE_box = np.average(TKE_box, axis=1, weights=lat_wgts)
+            TKE_avg = np.nanmean(TKE_box) # should I weight pressures too? How?
+            KE.append(TKE_avg)
+        save_file(exp, KE, 'KE')
 
-    print("plotting")
-    KE = open_file(exp, 'KE')
-    fig, ax = plt.subplots(figsize=(10,6))
-    ax.plot(len(KE), KE, color='k')
-    ax.set_xlim(1,len(KE))
-    ax.set_xlabel("Days Simulated")       
-    ax.set_ylabel('total KE', color='k')
-    plt.savefig(exp+'_spinup.pdf', bbox_inches = 'tight')
-    return plt.close()
+        print("plotting")
+        KE = open_file(exp, 'KE')
+        fig, ax = plt.subplots(figsize=(10,6))
+        ax.plot(len(KE), KE, color='k')
+        ax.set_xlim(1,len(KE))
+        ax.set_xlabel("Days Simulated")       
+        ax.set_ylabel('total KE', color='k')
+        plt.savefig(outdir+exp+'_spinup.pdf', bbox_inches = 'tight')
+        plt.close()
 
 def remove_uninterp(exp):
     print(datetime.now(), ' - interpolation processing')
@@ -113,20 +117,19 @@ def remove_uninterp(exp):
             except OSError as e:
                 print(e.strerror, ':', f)
 
-def delete_spinup(exp):
+def delete_spinup(indir):
     print(datetime.now(), ' - delete spin-up data')
     os.chdir(indir)
     folders = glob('*/spin*', recursive=True)
     for f in folders:
         try:
-            os.remove(f)
+            shutil.rmtree(f)
         except OSError as e:
             print(e.strerror, ':', f)
 
 def postprocess(exp):
     print(datetime.now(), ' - ', exp)
     os.chdir(indir + exp)
-    """
     runs = sorted(glob('run*'))
     n = len(runs) - 1
     
@@ -165,7 +168,7 @@ def postprocess(exp):
     nco.ncwa(input = exp+'_ut.nc', output = exp+'_utz.nc', options = ['-a lon'])
     nco.ncwa(input = exp+'_vt.nc', output = exp+'_vtz.nc', options = ['-a lon'])
     nco.ncwa(input = exp+'_Tt.nc', output = exp+'_Ttz.nc', options = ['-a lon'])
-    """
+
     # zonal means
     # NOTE THAT THE FOLLOWING ONLY SEEMS TO WORK ON GV3 OR GV4?
     print(datetime.now(), ' - zonal means')
@@ -190,19 +193,36 @@ def postprocess(exp):
 if __name__ == '__main__': 
     indir = '/disco/share/rm811/isca_data/'
     outdir = '/disco/share/rm811/processed/'
-    plevdir = '/home/links/rm811/Isca/postprocessing/plevel_interpolation/scripts'
-    analysisdir = '/home/links/rm811/scratch/PhD_Project_Isca'
+    plevdir = '/home/links/rm811/Isca/postprocessing/plevel_interpolation/scripts/'
+    analysisdir = '/home/links/rm811/scratch/PhD_Project_Isca/'
 
     nco = Nco(debug=True)
 
+    func = input('Do you want to a) postprocess, b) remove uninterpolated files, c) find TKE, d) back-calculate w, e) retrospectively extract variables, or f) delete spin-up data?')
+
+    exp = 'PK_e0v4z13_w15a4p300f800g50_h4000m2l25u65'
+            #['PK_e0v4z13_h4000m2l25u65',\
+            #'PK_e0v4z13_w15a4p300f800g50_h4000m2l25u65',\
+            #'PK_e0v4z13_w15a4p800f800g50_h4000m2l25u65']
+
+    if func == 'b':
+        remove_uninterp(exp)    
+    elif func == 'c':
+        find_TKE(outdir, analysisdir)  
+    elif func == 'e':
+        retrospective_calcs(indir, outdir)
+    elif func == 'f':
+        delete_spinup(indir)
+    else:
+        for i in range(len(exp)):
+            if func == 'a':
+                postprocess(exp[i])
+            elif func == 'd':
+                calc_w(outdir, exp[i])
+
+
     #sys.path.append(os.path.abspath(plevdir))
     #import run_plevel 
-
-    exp = ['PK_e0v4z13_h4000m2l25u65',\
-            'PK_e0v4z13_w15a4p300f800g50_h4000m2l25u65',\
-            'PK_e0v4z13_w15a4p800f800g50_h4000m2l25u65']
-    
-    #retrospective_calcs(indir, outdir)
 
     #print(datetime.now(), ' - calculating constants')
     #ds = xr.open_dataset('../atmos_daily_T42_p40.nc', decode_times=False)
@@ -213,14 +233,4 @@ if __name__ == '__main__':
     #p = ds.phalf
     #dp = np.diff(p)
     ##p = ds.pfull
-    ##dp = np.gradient(p)
-    
-    #for i in range(len(exp)):
-    #postprocess(exp)
-    remove_uninterp(exp)
-        #find_TKE(outdir, exp[i])
-        #calc_w(outdir, exp[i])
-
-    
-
-    
+    ##dp = np.gradient(p)   
