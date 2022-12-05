973368d4eef42aa2ab9afaada78803f023cf1c96
Regan Mudhar <rm811@maths2.ex.ac.uk>
2022-12-05 14:21:14 +0000

    zero wind line comparison

--- open_RA.py
+++ open_RA.py
@@ -2,6 +2,8 @@ from glob import glob
 import xarray as xr
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.colors as cm
+from datetime import datetime
 from open_winds import *
 from shared_functions import *
 from eofs.xarray import Eof
@@ -90,5 +92,46 @@ def SPV_finder():
         print('{0:.2f} ± {1:.2f} m/s'.format(SPV_mean, SPV_sd))
         print('{0:.2f} ± {1:.2f} / 100 days'.format(SSWs, err))
 
-SPV_finder()
+def uz_plot():
+    # Plots re-analysis NDJF mean zonal wind
+    print(datetime.now(), " - opening files")
+    folder = '/disco/share/rg419/ERA_5/daily_means_1979_to_2020/'
+    var = 'u_component_of_wind'
+    files = sorted(glob(folder+var+'*.nc'))
+    ds = xr.open_mfdataset(files, decode_times=True)
+    ds = ds.assign_coords({'month': ds.time.dt.month})
+    ds_NDJF = ds.where((ds.month <= 2)|(ds.month == 11), drop=True)
+    uz1 = ds_NDJF.u.drop_vars('month').mean('longitude').mean('time')
+    u_lvls = np.arange(-70, 100, 10)
+    p1 = ds.level
+    lat1 = ds.latitude
+
+    indir = '/disco/share/rm811/processed/'
+    exp = 'PK_e0v4z13_q6m2y45l800u200'
+    uz2 = xr.open_dataset(indir+exp+'_utz.nc', decode_times=False).ucomp[0]
+    p2 = uz2.pfull
+    lat2 = uz2.lat
+
+    print(datetime.now(), " - plotting")
+    fig, ax = plt.subplots(figsize=(6,6))
+    norm = cm.TwoSlopeNorm(vmin=min(u_lvls), vmax=max(u_lvls), vcenter=0)
+    csa = ax.contourf(lat1, p1, uz1, levels=u_lvls, norm=norm, cmap='RdBu_r')
+    cb  = fig.colorbar(csa, extend='both')
+    cb.set_label(label=r'NDJF zonal wind (m s$^{-2}$)', size='xx-large')
+    cb.ax.tick_params(labelsize='x-large')
+    csb = ax.contour(lat2, p2, uz2, colors='k', linewidths=1.5, alpha=0.25, levels=u_lvls)
+    csb.collections[list(u_lvls).index(0)].set_linewidth(3)
+    ax.set_ylabel('Pressure (hPa)', fontsize='xx-large')
+    ax.set_ylim(max(p2), min(p1))
+    ax.set_yscale('log')
+    ax.set_xlabel(r'Latitude ($\degree$N)', fontsize='xx-large')
+    ax.set_xlim(0, max(lat1))
+    ax.set_xticks([0, 20, 40, 60, 80], ['0', '20', '40', '60', '80'])
+    plt.tick_params(axis='both', labelsize = 'xx-large', which='both', direction='in')
+    plt.savefig('ERA5_NDJF_uz.pdf', bbox_inches = 'tight')
+    plt.show()
+    return plt.close()
+
+uz_plot()
+#SPV_finder()
 #t1, t2, v1, v2 = EOF_finder()
